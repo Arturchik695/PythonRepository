37311886a410c8919463e01773634bf97da2eca8
Arturchik <arturchik695@gmail.com>
2023-02-26 12:40:23 +0300

    first commit

--- DZ_11.py
+++ DZ_11.py
@@ -1,3 +1,10 @@
+# Задача №1
+a = ['red', 'green', 'blue']
+b = ['#FF0000', '#008000', '#0000FF']
+d = dict(zip(a, b))
+print(d)
+
+
 # Задача №2
 # a = {1: 10, 2: 20}
 # b = {3: 30, 4: 40}
@@ -12,21 +19,21 @@
 
 
 # Задача №3
-workers = {
-    "emp1": {"name": "John", "salary": 7500},
-    "emp2": {"name": "Emma", "salary": 8000},
-    "emp3": {"name": "Brad", "salary": 6500}
-}
-for x in workers:
-    print(x)
-    for y in workers[x]:
-        print("\t", y, ": ", workers[x][y], sep="")
-
-key = input("Введите ключ работника: ")
-salary = input("Для изменения зарплаты введите - salary: ")
-print(workers[key][salary])
-new_data = int(input("Укажите новую зарплату: "))
-workers[key][salary] = new_data
-print(workers[key])
+# workers = {
+#     "emp1": {"name": "John", "salary": 7500},
+#     "emp2": {"name": "Emma", "salary": 8000},
+#     "emp3": {"name": "Brad", "salary": 6500}
+# }
+# for x in workers:
+#     print(x)
+#     for y in workers[x]:
+#         print("\t", y, ": ", workers[x][y], sep="")
+#
+# key = input("Введите ключ работника: ")
+# salary = input("Для изменения зарплаты введите - salary: ")
+# print(workers[key][salary])
+# new_data = int(input("Укажите новую зарплату: "))
+# workers[key][salary] = new_data
+# print(workers[key])
 
 
