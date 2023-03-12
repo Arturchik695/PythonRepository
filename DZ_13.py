3af153b7a278912e167393470f504b162f31ba5f
Arturchik <arturchik695@gmail.com>
2023-03-12 16:33:14 +0300

    first commit

---
+++ DZ_13.py
@@ -0,0 +1,35 @@
+# Задача №2
+# def change_to_str(s, c_old, c_new):
+#     s2 = ""
+#     i = 0
+#     while i < len(s):
+#         if s[i] == c_old and i % 2 == 0:
+#             s2 += c_new
+#         else:
+#             s2 += s[i]
+#         i += 1
+#     return s2
+#
+#
+# str1 = "Я изучаю Nython. Мне нравится Nython. Nython очень интересный"
+# str2 = change_to_str(str1, "N", "P")
+# print("str1 = ", str1)
+# print("str2 = ", str2)
+
+
+# # Задача №3
+# s1 = "0123456789"
+# s2 = s1[:4] + s1[5:]
+# print("str1 =", s1)
+# print("str2 =", s2)
+
+
+# Задача №4
+# s1 = "Delete elements"
+# delete = "e"
+# print("s1: " + s1)
+# s2 = ""
+# for i in s1:
+#     if i != delete:
+#         s2 += i
+# print("s2: " + s2)
